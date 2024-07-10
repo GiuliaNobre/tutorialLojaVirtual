c001081ebf61b69e462c41bedbd21d5fa3a44baf
giulianobre <giuliarnobre@gmail.com>
2024-07-10 15:12:31 -0300

    Autenticação em PWA

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="TKx5Tl9XD7PUtXVD9gveFBL2"></passage-auth>
+  </div>
+</template>
+
+<style></style>
